228dbb13d52624a6d301d567ec5772d5944139a2
Vitor <vitor2071@gmail.com>
2025-12-05 10:39:25 -0300

    fix: Faz correções na função de extrato

--- models/accountBank.swift
+++ models/accountBank.swift
@@ -1,24 +1,34 @@
 class Account {
   let name: String
-  let numbAccount: String
+  let accountNumber: String
   private(set) var balance: Double
 
   init(name: String) {
     self.name = name
-    self.numbAccount = String(Int.random(in: 1000...4000))
+    self.accountNumber = String(Int.random(in: 1000...4000))
     self.balance = 0
   }
  
  func deposit(value: Double) {
-    if(value > 0) {
-      balance+=value
+    if value > 0 {
+      balance += value
+      print("✅ Depósito de R$ 500.0 realizado com sucesso.")
     } else { 
       print("Valor inválido. Tente novamente.")
     }
   }
 
-  func dataClient() {
-    print("Name: \(self.name)\nNumber account: \(self.numbAccount)\nBalance: \(self.balance)")
+  func statementBank() {
+    print("Extrato de \(self.name) (Conta \(self.account)): Saldo Atual: R$ \(self.balance)")
+  }
+
+  func withdraw(value: Double) { 
+    if self.balance >= value {
+      self.balance -= value
+      print("✅ Saque de R$ \(value) realizado. Novo saldo: R$ \(self.balance).")
+    } else {
+      print("Saldo insuficiente para sacar R$ \(value).")
+    }
   }
 }
 
