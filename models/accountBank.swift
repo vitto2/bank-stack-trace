088075fa810f300ca430d62c26aa780c3bdc7924
Vitor <vitor2071@gmail.com>
2025-12-05 09:37:57 -0300

    feat: corrige func deposit e cria função que retorna dados completos do cliente

--- models/accountBank.swift
+++ models/accountBank.swift
@@ -1,14 +1,24 @@
 class Account {
   let name: String
   let numbAccount: String
-  private(set) balance: Double
+  private(set) var balance: Double
 
   init(name: String) {
     self.name = name
-    self.numbAccount = Int.random(in: 1...10)
+    self.numbAccount = String(Int.random(in: 1000...4000))
     self.balance = 0
   }
-
  
+ func deposit(value: Double) {
+    if(value > 0) {
+      balance+=value
+    } else { 
+      print("Valor inválido. Tente novamente.")
+    }
+  }
+
+  func dataClient() {
+    print("Name: \(self.name)\nNumber account: \(self.numbAccount)\nBalance: \(self.balance)")
+  }
 }
 
